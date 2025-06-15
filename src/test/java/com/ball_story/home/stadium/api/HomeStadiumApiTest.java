1cdbbda2c0fef85f3b5d66381065c9f846413f92
mirea70 <deae670@daum.net>
2025-06-15 21:45:06 +0900

    refactor: HomeStadium Create Test Code With h2

--- src/test/java/com/ball_story/home/stadium/api/HomeStadiumApiTest.java
+++ src/test/java/com/ball_story/home/stadium/api/HomeStadiumApiTest.java
@@ -1,16 +1,36 @@
 package com.ball_story.home.stadium.api;
 
 import com.ball_story.common.enums.Team;
-import com.ball_story.home.account.dto.AccountJoinRequest;
 import com.ball_story.home.stadium.dto.StadiumCreateRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Slf4j
 public class HomeStadiumApiTest {
-    RestClient restClient = RestClient.create("http://localhost:8080");
+    @LocalServerPort
+    private int port;
+    private RestClient restClient;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeAll
+    public void setup() {
+        restClient = RestClient.create("http://localhost:" + port);
+    }
 
     @Test
     public void createTest() {
@@ -21,6 +41,10 @@ public class HomeStadiumApiTest {
         ));
 
         assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+
+        Integer count = jdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM home_stadium", Integer.class);
+        log.info("Data count = {}", count);
     }
 
     private ResponseEntity<Void> create(StadiumCreateRequest request) {
